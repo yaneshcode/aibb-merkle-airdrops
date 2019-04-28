d453f34f9267ea5d28d3113667b123f9731f9667
yanesh <yaneshcode@gmail.com>
2019-04-28 12:52:03 -0400

    contract

--- contracts/aibbMerkleAirdrop.sol
+++ contracts/aibbMerkleAirdrop.sol
@@ -3,13 +3,30 @@ pragma solidity ^0.5.0;
 contract aibbMerkleAirdrop {
 
   address public owner;
+  bytes32 public root;
+  mapping(address => bool) public claimedUser;
 
-  constructor() public {
-    owner = msg.sender;
+  constructor (bytes32 _root) public {
+      owner = msg.sender;
+      root = _root;
   }
 
-  modifier onlyOwner() {
-    require(msg.sender == owner);
-  _;}
+  function claimToken ( bytes32[] memory _proof, uint256 index, address recipient) view public {
+      require(claimedUser[(recipient)] == false);
+      bytes32 node;
 
+      if (index % 2 == 0) {
+          node = keccak256(abi.encodePacked(recipient, _proof[0]));
+      } else {
+          node = keccak256(abi.encodePacked(_proof[0], recipient));
+      }
+
+
+      for (uint16 i = 1; i < _proof.length; i++) {
+      if (i % 2 == 0) {
+          node = keccak256(abi.encodePacked(_proof[i], node));
+      }
+          node = keccak256(abi.encodePacked(node, _proof[i]));
+      }
+  }
 }
--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,5 +1,5 @@
 var aibbMerkleAirdrop = artifacts.require("./aibbMerkleAirdrop.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(aibbMerkleAirdrop);
+  deployer.deploy(aibbMerkleAirdrop, "0xca35b7d915458ef540ade6068dfe2f44e8fa733c");
 }
